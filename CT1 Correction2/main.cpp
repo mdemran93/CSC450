369767d898bb6c38344de557d4a9194163fb8434
Md Emran <142960369+mdemran93@users.noreply.github.com>
2023-10-16 22:12:47 -0400

    Add files via upload

---
+++ CT1 Correction2/main.cpp
@@ -0,0 +1,32 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Oct 16, 2023
+ *      Author: Emran
+ */
+
+#include<iostream>
+#include<conio.h>
+
+//Standard namespace declaration
+using namespace std;
+//Main Function
+int main()
+{
+double Mymoney = 1000.50; //this should be printed out
+
+//Standard Ouput Statement
+
+cout<<"Please be sure to correct all syntax errors in this program"<<endl;
+
+cout<<"I have corrected all errors for this program. "<<endl;
+
+cout<<" The total amount of money available is = "<<Mymoney <<endl;
+
+// Wait For Output Screen
+//Main Function return Statement
+return 0;
+}
+
+
+
