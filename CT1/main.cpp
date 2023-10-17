12816c523cb53df9162309569299ccc74bc5cac7
Md Emran <142960369+mdemran93@users.noreply.github.com>
2023-10-16 21:12:20 -0400

    Add files via upload

---
+++ CT1/main.cpp
@@ -0,0 +1,28 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Oct 14, 2023
+ *      Author: Emran
+ */
+
+#include <iostream>
+#include <string>
+
+int main() {
+
+    std::string firstName = "Md";
+    std::string lastName = "Emran";
+    std::string streetAddress = "189 jesper street";
+    std::string city = "Paterson";
+    std::string zipCode = "07189+";
+
+
+    std::cout << "First Name: " << firstName << std::endl;
+    std::cout << "Last Name: " << lastName << std::endl;
+    std::cout << "Street Address: " << streetAddress << std::endl;
+    std::cout << "City: " << city << std::endl;
+    std::cout << "Zip Code: " << zipCode << std::endl;
+
+    return 0;
+}
+
